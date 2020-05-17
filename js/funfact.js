1d0ca06b047403794c65cdbdd8f5673c758b4437
Ashley Chu <ashleymchu@yahoo.com>
2020-05-17 00:03:23 -0700

    updated advice

--- js/funfact.js
+++ js/funfact.js
@@ -1,1 +1,13 @@
-var elements[] = ["https://ashleyc.netlify.app/"];
+// var elements[] = ["https://ashleyc.netlify.app/"];
+
+function addAdvice(){
+    if ((document.getElementById('advice-reveal').style.display) == ("block")) {
+      document.getElementById('advice-reveal').style.display = null;
+    } else {
+    document.getElementById('advice-reveal').style.display = "block";
+  }
+}
+
+function addFact() {
+  
+}
