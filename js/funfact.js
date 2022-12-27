00d641bc0edda3e14c02c857381bfac63b952cf3
Ashley Chu <ashleymchu@yahoo.com>
2022-12-26 22:25:16 -0800

    adding in extra proj

--- js/funfact.js
+++ js/funfact.js
@@ -9,13 +9,19 @@ function addAdvice(){
 }
 
 function addFact() {
-  // var facts = ["Pienapple belongs on pizza.", "I've watched the whole Avatar series 5x.", "Wyoming only has two escalators.", "This tab was inspired by CS61B's sum of all human knowledge."];
+  var facts = ["Pienapple belongs on pizza.", "You look nice today :)", "I've watched the whole Avatar series 5x.", "Wyoming only has two escalators.", "Boba should be considered a soup.", "This tab was inspired by CS61B's sum of all human knowledge."];
   if ((document.getElementById('funfact-reveal').style.display) == ("block")) {
     document.getElementById('funfact-reveal').style.display = null;
   } else {
-    // var index = Math.floor(Math.random() * (facts.length-1));
-    // console.log(index);
-    // document.getElementById('ffspan').innerHTML = facts[index];
+    var index = Math.floor(Math.random() * (facts.length-1));
+    console.log(index);
+    document.getElementById('ffspan').innerHTML = facts[index];
     document.getElementById('funfact-reveal').style.display = "block";
   }
+}
+
+function addProject() {
+  var sites = ["https://arkgnaaerkgnajengajngreajngaejngaenhatrklhnalknth.netlify.app/", "https://bitbybit.netlify.app/", "https://justthebeginningheheheehe.netlify.app/", "https://mommasday21.netlify.app/", "https://walmeals.netlify.app/", "https://60b6f97f95a71441e64c5570--inertiamobile.netlify.app/", "https://swetreatwoohoo.netlify.app/", "https://ucbswebsite.netlify.app/", "https://ilovemydoggos.netlify.app/", "https://kwkphotoalblum.netlify.app/", "https://happymothersday.netlify.app/", "https://alamedaasa.netlify.app/", "https://ashleychu.netlify.app/" ];
+  var index = Math.floor(Math.random() * (sites.length));
+  open(sites[index]);
 }
